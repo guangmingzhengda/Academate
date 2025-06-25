93fe8599c546d63fcb9a6f27416cd3e7c2274aa3
RyotoBUAA <1164754246@qq.com>
2025-06-25 10:08:49 +0800

    feat: fast deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,9 +1,9 @@
 npm run build
 tar -cvf ../academate.tar -C ../dist .
-scp -v ../academate.tar academate@123.56.50.152:/var/www/academate/academate.tar
-ssh academate@123.56.50.152 'bash -s' << 'ENDSSH'
+scp -v ../academate.tar buaa@10.251.254.146:~/academate.tar
+ssh buaa@10.251.254.146 'bash -s' << 'ENDSSH'
 echo 'Unzipping'
-cd /var/www/academate
+cd ~
 rm -rf academate/*
 tar -xvf academate.tar -C academate
 exit
